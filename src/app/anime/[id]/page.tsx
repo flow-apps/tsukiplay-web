82af399338bb3dfb67f7bf0e992587386dbc4026
pedrohenriquebraga <pedrobraga693@gmail.com>
2026-07-29 11:50:07 -0300

    fix og tags

--- src/app/anime/[id]/page.tsx
+++ src/app/anime/[id]/page.tsx
@@ -1,69 +1,68 @@
-"use client";
-
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import { api } from "@/services/api";
-import { Anime, AnimeSearchResponse } from "@/types/anime";
-import { TopAnimeItem, TopResponse } from "@/types/top";
-import Head from "next/head";
-import { useParams } from "next/navigation";
+import { TopAnimeItem } from "@/types/top";
 import styles from "@/styles/anime.module.css";
-import React, { useEffect, useState } from "react";
+import { Metadata } from "next";
 
 interface IAnimeResponse {
   data: TopAnimeItem;
 }
 
-const AnimePage: React.FC = () => {
-  const [loading, setLoading] = useState(true);
-  const [anime, setAnime] = useState<TopAnimeItem>();
-  const { id } = useParams();
+async function getAnimeData(id: string): Promise<TopAnimeItem> {
+  const {
+    data: { data },
+  } = await api.get<IAnimeResponse>(`/anime/${id}/full`);
+  return data;
+}
 
-  useEffect(() => {
-    (async () => {
-      if (!id) return;
+type PageProps = {
+  params: { id: string };
+};
 
-      console.log(id);
+export async function generateMetadata({
+  params,
+}: PageProps): Promise<Metadata> {
+  const { id } = await params;
+  const data = await getAnimeData(id);
 
-      const {
-        data: { data },
-      } = await api.get<IAnimeResponse>(`/anime/${id}/full`);
+  const title = "Detalhes sobre " + (data.title_english || data.title);
+  const description = data.synopsis;
 
-      setAnime(data);
-      setLoading(false);
-    })();
-  }, [id]);
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      images: data.images.webp.large_image_url,
+    },
+  };
+}
 
-  if (loading) return <p>Loading...</p>;
+const AnimePage = async ({ params }: PageProps) => {
+  const { id } = await params;
+  const anime = await getAnimeData(id);
 
   return (
     <>
-      <Head>
-        <title>Detalhes sobre {anime?.title_english || anime?.title}</title>
-        <meta property="og:title" content={anime?.title_english || anime?.title} />
-        <meta
-          property="og:description"
-          content={anime?.synopsis}
-        />
-        <meta property="og:image" content={anime?.images.webp.large_image_url}/>
-      </Head>
       <Header />
       <main className={styles.mainContainer}>
         <div className={styles.animeCard}>
           <img
-            src={anime?.images.webp.large_image_url}
-            alt={anime?.title_english || anime?.title}
+            src={anime.images.webp.large_image_url}
+            alt={anime.title_english || anime.title}
             className={styles.animeImage}
           />
           <h1 className={styles.animeTitle}>
-            {anime?.title_english || anime?.title}
+            {anime.title_english || anime.title}
           </h1>
           <div className={styles.buttonContainer}>
             <a
               href={`tsukiplay://anime/${id}`}
               className={styles.appButton}
             >
-                Abrir no TsukiPlay
+              Abrir no TsukiPlay
             </a>
             <a
               href="https://play.google.com/store/apps/details?id=com.flowapps.AnimeApp"
